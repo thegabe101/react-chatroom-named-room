442caa4e9f52fda578aaf3504ef115ef64a29f26
Rayncor <sowa332@gmail.com>
2023-01-18 10:15:33 -0800

    messages successfully posting to firebase, but read write rules using auth are not working so data is insecure

--- src/components/chat.js
+++ src/components/chat.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { useState } from 'react';
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { auth, db } from '../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export const Chat = (props) => {
-    const [newMessage, setNewMessage] = useState("")
+    const [newMessage, setNewMessage] = useState("");
+    // const [user, loading, error] = useAuthState(auth);
     const { room } = props;
     const messageRef = collection(db, 'messages');
 
     const handleSubmit = async (e) => {
+        // console.log(user);
+        console.log(auth.currentUser)
         e.preventDefault();
         if (newMessage === '') return;
 
